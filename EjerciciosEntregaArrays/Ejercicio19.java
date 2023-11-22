e38bc2157c5ff620174cd99218aeceb40cc3c88c
MissyMogara <monicagara97@gmail.com>
2023-11-22 11:56:28 +0100

    haciendo el ejercicio 20

--- EjerciciosEntregaArrays/Ejercicio19.java
+++ EjerciciosEntregaArrays/Ejercicio19.java
@@ -1,6 +1,55 @@
 package EjerciciosEntregaArrays;
 
+import java.util.Scanner;
+
+import java.util.Arrays;
+
 public class Ejercicio19 {
+    /**
+     * Función que crea un array y lo devuelve
+     * 
+     * @return
+     */
+    public static int[] pila() {
+        int pila[] = new int[20];
+        return pila;
+    }
+
+    /**
+     * Proceso que inserta valores en un array pila
+     * 
+     * @param pila
+     * @param valor
+     * @param posicion
+     */
+    public static void insertarValorPila(int[] pila, int valor, int posicion) {
+        for (int i = 0; i <= posicion; i++) {
+            if (pila[i] == 0) {
+                pila[i] = valor;
+            }
+        }
+    }
+
+    /**
+     * Proceso que quita valores en un array pila
+     * 
+     * @param pila
+     * @param valor
+     * @param posicion
+     */
+    public static int quitarValorPila(int[] pila, int valor, int posicion) {
+        int valorQuitado;
+        if (pila[0] == 0) {
+            System.out.println("Error: No se puede borrar ningún elemento de la pila porque no tiene elementos.");
+
+        } else {
+            valorQuitado = pila[posicion - 1];
+            pila[posicion - 1] = 0;
+            return valorQuitado;
+        }
+        return 0;
+    }
+
     /*
      * Implementación de una Pila con un vector de 20 elementos. Una Pila se basa en
      * el concepto “Primero en entrar Último en salir”. De modo que se saca siempre
@@ -8,4 +57,80 @@ public class Ejercicio19 {
      * insertar elemento en la pila, sacar elemento de la pila (lo muestra y lo
      * quita), comprobar si la pila está llena.
      */
+    public static void main(String[] args) {
+        /*
+         * Empezamos inicializando el scanner y el array llamando a la función de crear
+         * pila, ademas inicializamos la variable opción del menú, y otra variable para
+         * controlar la posición de los valores en la pila
+         */
+        Scanner sc = new Scanner(System.in);
+        int array[] = pila();
+        int opcion = 0;
+        int valor = 0;
+        int posicion = 0;
+
+        /* Realizamos un menú */
+        while (opcion != 4) {
+            /* Enseñamos las opciones al usuario */
+            System.out.println("Elige una opción:");
+            System.out.println("1. Insertar elemento en la pila.");
+            System.out.println("2. Borrar elemente en la pila.");
+            System.out.println("3. Comprobar la capacidad de la pila.");
+            System.out.println("4. Salir.");
+            /* Le pedimos al usuario que inserte una opción */
+            opcion = Integer.parseInt(sc.nextLine());
+
+            switch (opcion) {
+                case 1:
+                    /* Esta opcion llamara a un proceso para añadir un valor a la ultima posicion */
+                    System.out.println("Inserta un valor que no sea '0':");
+                    valor = Integer.parseInt(sc.nextLine());
+                    if (valor == 0) {
+                        /* Si es 0, no es válido puesto a que es un valor vacio */
+                        System.out.println("Error: Por favor inserta un valor que no sea '0'.");
+                    } else if (posicion == array.length) {
+                        /*
+                         * Si la posicion es igual a la longitud del array entonces ya estamos en el
+                         * limite
+                         */
+                        System.out.println("Error: La pila esta llena.");
+                    } else {
+                        /* Sino ocurre ninguna de las anteriores entonces insertamos el valor */
+                        insertarValorPila(array, valor, posicion);
+                        posicion += 1;
+                    }
+                    break;
+                case 2:
+                    /*
+                     * Llamamos a una funcion que borrara el ultimo valor añadido y ademas lo
+                     * mostrará
+                     */
+                    System.out.println("El valor borrado es: " + quitarValorPila(array, valor, posicion));
+                    posicion -= 1;
+                    break;
+                case 3:
+                    /* Cuando se eliga esta opcion comprobaremos si esta llena o no la pila */
+                    if (posicion == array.length) {
+
+                        System.out.println("-La pila esta llena:");
+                        System.out.println(Arrays.toString(array));
+                    } else {
+
+                        System.out.println("-La pilla aún no esta llena:");
+                        System.out.println(Arrays.toString(array));
+                    }
+
+                    break;
+
+                default:
+                    if (opcion == 4) {
+                        System.out.println("Adiós");
+                    }
+                    break;
+            }
+
+        }
+        sc.close();
+    }
+
 }
---
+++ EjerciciosEntregaArrays/Ejercicio20.java
@@ -0,0 +1,250 @@
+package EjerciciosEntregaArrays;
+
+import java.util.Scanner;
+
+public class Ejercicio20 {
+    /**
+     * Proceso que se recorre una matriz y la pinta
+     * 
+     * @param array
+     */
+    public static void pintarArrayBidimensional(char[][] array) {
+        // Recorre las filas del array bidimensional
+        for (int i = 0; i < array.length; i++) {
+            // Recorre las columnas de la fila actual
+            for (int j = 0; j < array[i].length; j++) {
+                // Imprime el valor del array
+                System.out.print(array[i][j] + "\t");
+            }
+            // Nueva línea después de cada fila
+            System.out.println();
+        }
+    }
+
+    /**
+     * Rellena una matriz con el caracter '-'
+     * 
+     * @param array
+     */
+    public static void rellenarArrayBi(char array[][]) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = '-';
+            }
+
+        }
+
+    }
+
+    /**
+     * Esta funcion devuelve un valor true o false en funcion de si el movimiento es
+     * erroneo o no, si devuelve true aplica el movimiento
+     * 
+     * @param tablero
+     * @param posicionX
+     * @param posicionY
+     * @param caracter
+     * @return
+     */
+    public static boolean insertarXO(char tablero[][], int posicionX, int posicionY, char caracter) {
+
+        if (!(posicionX >= tablero.length) || !(posicionY >= tablero[0].length)) {
+            if (tablero[posicionY][posicionX] == '-') {
+                if (caracter == 'x' || caracter == 'o') {
+                    tablero[posicionY][posicionX] = caracter;
+                    return true;
+                } else {
+                    System.out.println("Error: Inserta 'x' u 'o'.");
+                    return false;
+                }
+            } else {
+                System.out.println("Error: La posición ya esta ocupada.");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Función que ayuda a la funcion finPartida a comprobar si algun jugador ha
+     * ganado
+     * 
+     * @param x
+     * @param o
+     * @return
+     */
+    public static int victoriaJugador(int x, int o) {
+        if (x == 3) {
+            return 1;
+        } else if (o == 3) {
+            return 2;
+        }
+        return 0;
+    }
+
+    /**
+     * funcion que devuelve un entero en funcion del jugador que gane, si es de es
+     * empate
+     * 
+     * @param tablero
+     * @return
+     */
+    public static int finPartida(char[][] tablero) {
+
+        int x = 0;
+        int o = 0;
+        /* Comprueba las columnas */
+        for (int i = 0; i < tablero.length; i++) {
+
+            for (int j = 0; j < tablero.length; j++) {
+                if (tablero[j][i] == 'x') {
+                    x += 1;
+                } else if (tablero[j][i] == 'o') {
+                    o += 1;
+                }
+
+            }
+
+            /* Acaba la partida cuando alguien hace un 3 en raya */
+
+            if (victoriaJugador(x, o) == 0) {
+                x = 0;
+                o = 0;
+            } else if (victoriaJugador(x, o) != 0) {
+                return victoriaJugador(x, o);
+            }
+
+        }
+
+        for (int i = 0; i < tablero.length; i++) {
+            /* Comprueba las filas */
+            for (int j = 0; j < tablero.length; j++) {
+                if (tablero[i][j] == 'x') {
+                    x += 1;
+                } else if (tablero[i][j] == 'o') {
+                    o += 1;
+                }
+
+            }
+            /* Acaba la partida cuando alguien hace un 3 en raya */
+            if (victoriaJugador(x, o) == 0) {
+                x = 0;
+                o = 0;
+            } else if (victoriaJugador(x, o) != 0) {
+                return victoriaJugador(x, o);
+            }
+        }
+        /*Comprueba la diagonal */
+        for (int i = 0; i < tablero.length; i++) {
+
+            for (int j = 0; j < tablero.length; j++) {
+
+                if (j + i == tablero[i].length-1) {
+                    
+                    
+                    
+                }
+               
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         * Vamos a realizar el típico juego del 3 en raya, donde habrá dos jugadores que
+         * tengan que hacer el 3 en raya, los signos serán el X y el O, cuando haya una
+         * posición vacía habrá un –
+         * El tablero de juego, será una matriz de 3×3 de char.
+         * El juego termina cuando uno de los jugadores hace 3 en raya o si no hay más
+         * posiciones que poner.
+         * Primero juega una posición un jugador y luego el otro.
+         * El juego debe pedir las posiciones donde el jugador actual quiera poner su
+         * marca ( [0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2] ), esta
+         * debe ser validada y por supuesto que no haya una marca ya puesta.
+         */
+
+        /* Declaramos y rellenamos el tablero */
+        Scanner sc = new Scanner(System.in);
+        char tablero3enRaya[][] = new char[3][3];
+        rellenarArrayBi(tablero3enRaya);
+        pintarArrayBidimensional(tablero3enRaya);
+        /* Declaramos una variable booleana para comprobar cuando se acaba la partida */
+        boolean acabaPartida = false;
+        int posicionX = 0;
+        int posicionY = 0;
+        int turnos = 0;
+        int turnojugador = 1;
+        boolean comprobacion = false;
+        System.out.println("¡Bienvenido a el tres en raya edición ULTIMATE!");
+
+        while (acabaPartida == false) {
+            turnos += 1;
+            switch (turnojugador) {
+                case 1:
+                    /* Turno del jugador 1 */
+                    System.out.println("Turno " + turnos + " del jugador 1 'x':");
+
+                    /* Le pedimos que inserte la posición X */
+                    System.out.println("Inserta la posición del eje X que quieres ocupar:");
+                    posicionX = Integer.parseInt(sc.nextLine());
+
+                    /* Le pedimos que inserte la posición Y */
+                    System.out.println("Inserta la posición del eje Y que quieres ocupar:");
+                    posicionY = Integer.parseInt(sc.nextLine());
+
+                    /* Pintar resultado */
+                    comprobacion = insertarXO(tablero3enRaya, posicionX, posicionY, 'x');
+                    pintarArrayBidimensional(tablero3enRaya);
+
+                    /* Pasamos el turno al jugador 2 */
+                    if (comprobacion == true) {
+                        turnojugador = 2;
+                    } else {
+                        turnos -= 1;
+                    }
+                    /* Comprobamos si se acabo la partida */
+                    if (finPartida(tablero3enRaya) != -1) {
+                        acabaPartida = true;
+                    }
+                    break;
+                case 2:
+                    /* Turno del jugador 2 */
+                    System.out.println("Turno " + turnos + " del jugador 2'o':");
+
+                    /* Le pedimos que inserte la posición X */
+                    System.out.println("Inserta la posición del eje X que quieres ocupar:");
+                    posicionX = Integer.parseInt(sc.nextLine());
+
+                    /* Le pedimos que inserte la posición Y */
+                    System.out.println("Inserta la posición del eje Y que quieres ocupar:");
+                    posicionY = Integer.parseInt(sc.nextLine());
+
+                    /* Pintar resultado */
+                    comprobacion = insertarXO(tablero3enRaya, posicionX, posicionY, 'o');
+                    pintarArrayBidimensional(tablero3enRaya);
+
+                    /* Pasamos el turno al jugador 1 */
+                    if (comprobacion == true) {
+                        turnojugador = 1;
+                    } else {
+                        turnos -= 1;
+                    }
+                    /* Comprobamos si se acabo la partida */
+                    if (finPartida(tablero3enRaya) != -1) {
+                        acabaPartida = true;
+                    }
+                    break;
+
+                default:
+                    System.out.println("Se acabó la partida");
+                    break;
+            }
+
+        }
+
+    }
+}
