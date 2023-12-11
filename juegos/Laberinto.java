b3f0926563ef1aca39c15be744594c06d1579eda
MissyMogara <monicagara97@gmail.com>
2023-12-11 10:20:02 +0100

    terminado ejercicio laberinto

--- juegos/Laberinto.java
+++ juegos/Laberinto.java
@@ -1,5 +1,7 @@
 package juegos;
 
+import java.util.Scanner;
+
 /*En este ejercicio vamos a implementar un juego de un laberinto. 
 Para ello nos crearemos una matriz de 20 x 20 caracteres. 
 La matriz será un eje de coordenadas donde la esquina superior izquierda será el inicio del 
@@ -47,7 +49,7 @@ public class Laberinto {
     }
 
     /**
-     * Proceso para pintar un arraybidimensional
+     * Proceso para pintar una matriz
      * 
      * @param matriz
      */
@@ -69,7 +71,7 @@ public class Laberinto {
      * 
      * @param matriz
      */
-    public static void rellenarTablero(String matriz[][]) {
+    public static void rellenarTablero(String matriz[][], int y, int x) {
         try {
 
             for (int i = 0; i < matriz.length; i++) {
@@ -86,6 +88,8 @@ public class Laberinto {
                 }
 
             }
+            /* Pintamos al personaje */
+            matriz[y][x] = "@";
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
@@ -98,11 +102,10 @@ public class Laberinto {
      * 
      * @param matriz tablero
      */
-    public static void generarSalida(String matriz[][]) {
+    public static void generarSalida(String matriz[][], int salida[]) {
         int y = generarAleatorio(0, matriz.length - 1);
         int x = 0;
         int aleatorio = -1;
-
         /* Comprobamos si el eje y esta en 0 o en su máxima longitud */
         if (y == 0 || y == matriz.length - 1) {
             if (y == 0) {
@@ -135,17 +138,196 @@ public class Laberinto {
             }
         }
         matriz[y][x] = "#";
+        salida[0] = y;
+        salida[1] = x;
+
+    }
+
+    /**
+     * Este proceso mueve al personaje
+     * 
+     * @param y      posicion "y" del personaje
+     * @param x      posicion "x" del personaje
+     * @param matriz tablero
+     * @param num    movimiento
+     */
+    public static void moverPersonaje(int y, int x, String matriz[][], int num) throws Exception {
+
+        if (num < 5 && num > 0) {
+
+            if (x == 0 || x == matriz[0].length - 1) {
+                matriz[y][x] = "|";
+            } else if ((y == 0 || y == (matriz.length - 1)) && x != 0) {
+                matriz[y][x] = "-";
+            } else {
+                matriz[y][x] = ".";
+            }
+        }
+
+        switch (num) {
+            case 1: // Derecha
+
+                if (x + 1 > 19) {
+                    throw new Exception("Te has salido de la pantalla, has perdido.");
+                }
+
+                matriz[y][x + 1] = "@";
+
+                break;
+
+            case 2: // Izquierda
+
+                if (x - 1 < 0) {
+                    throw new Exception("Te has salido de la pantalla, has perdido.");
+                }
+
+                matriz[y][x - 1] = "@";
+
+                break;
+
+            case 3: // Abajo
+
+                if (y + 1 > 19) {
+                    throw new Exception("Te has salido de la pantalla, has perdido.");
+                }
+
+                matriz[y + 1][x] = "@";
+
+                break;
+
+            case 4: // Arriba
+
+                if (y - 1 < 0) {
+                    throw new Exception("Te has salido de la pantalla, has perdido.");
+                }
+
+                matriz[y - 1][x] = "@";
+
+                break;
+
+            default:
+                System.out.println("Error inserta un número entre 1 o 4.");
+                break;
+        }
+
+    }
+
+    /**
+     * Función que devuelte si la partida ha terminado (true) o aun sigue (false)
+     * 
+     * @param y      posicion "y" del personaje
+     * @param x      posicion "x" del personaje
+     * @param salida array salida
+     * @param bomba  array bomba
+     * @return
+     */
+    public static boolean condicionAcabaPartida(String tablero[][], int y, int x, int salida[], int bomba[]) {
+        if (y == salida[0] && x == salida[1]) {
+            System.out.println("Has llegado a la meta!");
+            return true;
+        } else if (y == bomba[0] && x == bomba[1]) {
+            tablero[bomba[0]][bomba[1]] = "x";
+            System.out.println();
+            pintarMatriz(tablero);
+            System.out.println("Te ha explotado una bomba! has perdido.");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Proceso que genera una bomba en el tablero
+     * 
+     * @param tablero
+     * @param bomba
+     * @param salida
+     */
+    public static void generarBomba(String tablero[][], int bomba[], int salida[]) {
+
+        boolean generacionBuena = false;
+
+        while (generacionBuena == false) {
+
+            bomba[0] = generarAleatorio(0, tablero.length - 1);
+
+            bomba[1] = generarAleatorio(0, tablero[0].length - 1);
+
+            if (!(bomba[0] == salida[0] && bomba[1] == salida[1]) && !(bomba[0] == 0 && bomba[1] == 0)) {
+
+                generacionBuena = true;
+
+            }
+
+        }
+
     }
 
     public static void main(String[] args) {
 
-        String tablero[][] = new String[20][20];
-        rellenarTablero(tablero);
+        /* Scanner que nos pedirá el movimiento y el movimiento */
+        Scanner sc = new Scanner(System.in);
+        int movimiento = 0;
 
-        generarSalida(tablero);
+        /* Posición del personaje */
+        int posicionX = 0;
+        int posicionY = 0;
+
+        /* Condicion de final de partida */
+        boolean finalDePartida = false;
+
+        /* Salida, tablero, rellenar tablero y pintarlo */
+        String tablero[][] = new String[20][20];
+        int salida[] = new int[2];
+        int bomba[] = new int[2];
+
+        rellenarTablero(tablero, posicionY, posicionX);
+
+        generarSalida(tablero, salida);
+
+        generarBomba(tablero, bomba, salida);
 
         pintarMatriz(tablero);
 
+        System.out.println(bomba[0]);
+        System.out.println(bomba[1]);
+        try {
+            /* Bucle para que podamos jugar hasta que se acabe la partida */
+            while (finalDePartida == false) {
+
+                /* Opciones de movimiento */
+                System.out.println("Inserta el movimiento:");
+                System.out.println("1.Derecha");
+                System.out.println("2.Izquierda");
+                System.out.println("3.Abajo");
+                System.out.println("4.Arriba");
+
+                /* Pedir movimiento al usuario en formato número */
+                movimiento = Integer.parseInt(sc.nextLine());
+
+                moverPersonaje(posicionY, posicionX, tablero, movimiento);
+
+                /* Cambiamos los valores de la posición del personaje */
+                if (movimiento == 1) {
+                    posicionX += 1;
+                } else if (movimiento == 2) {
+                    posicionX -= 1;
+                } else if (movimiento == 3) {
+                    posicionY += 1;
+                } else if (movimiento == 4) {
+                    posicionY -= 1;
+                } else {
+
+                }
+
+                /* Pintamos los cambios */
+                pintarMatriz(tablero);
+                finalDePartida = condicionAcabaPartida(tablero, posicionY, posicionX, salida, bomba);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+        sc.close();
     }
 
 }
